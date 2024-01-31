97d063037811ae4dd985eecc33fe8b42cb4ae740
Slawomir Was <was.slawek@gmail.com>
2024-01-31 18:58:23 +0100

    Cookies 1: added workflow

---
+++ CookiesRecipeApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+var cookiesRecipeBook = new CookiesRecipesApp();
+cookiesRecipeBook.Run();
+
+class CookiesRecipesApp
+{
+	private readonly RecipesRepository _recipesRepository;
+	private readonly RecipesUserInteraction _recipesUserInteraction;
+
+    public CookiesRecipesApp()
+    {
+        
+    }
+    public void Run()
+	{
+	}
+}
